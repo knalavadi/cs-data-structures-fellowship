21ece8409c375b44c930b77f1564f763b0ba7dba
Krisha Nalavadi <krishanalavadi@Krishas-MacBook-Air.local>
2016-08-28 12:23:13 -0700

    uploading homework

---
+++ dicusion_questions.py
@@ -0,0 +1,47 @@
+'''
+Discussion Questions
+
+Runtime:
+1.) Finding an elephant inba box of animal crackers in the worst case scenario would 
+require looking through the entire box, which would be O(n). 
+
+2.) 
+
+O(1)
+O(logn)
+
+O(n)
+O(nlogn)
+
+O(n^2)
+O(2^n)
+
+
+Stacks and Queues:
+1.) Stack, Queue, stack 
+2.) Ticketing systems, drive thru 
+3.) dismantling a stacked display, pez dispenser 
+
+trees:
+breadth first
+1.) italian, indian, mexican
+italian lasagna 
+italian pizza
+indian tikka masala 
+indian saag 
+mexican burrito 
+
+depth first 
+2.)italian, indian, mexican 
+italian lasagna
+italian pizza 
+italian pizza sicialian 
+italian pizza new york style 
+italian pizza chicago style
+
+3.)
+Binary trees always have two child nodes, even if they are None. 
+
+
+
+'''
